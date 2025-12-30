d68d0a1584a23a8cf1b4cb59993f0285cbf8e18e
melodimistis <azaralfy01@gmail.com>
2025-12-30 19:16:56 +0700

    add DataItems

--- VendingMachine.py
+++ VendingMachine.py
@@ -2,16 +2,16 @@ from DataItems import coins, usr, items_equipment, items_food
 
 print('Hello Traveler! Welcome to the Medieval Vending Machine.')
 input_ask = input('Can i help you with something today? (I want to buy something/No thanks, maybe next time):').strip().lower()
-if input_ask in ("I want", 'i want', 'i', 'I', 'yes', 'Yes', 'I want to buy something'):
+if input_ask in ("I want", 'i want', 'y', 'Y', 'yes', 'Yes', 'I want to buy something'):
     print('Great! Let me assist you with that.')
 else:
-    print('Okay the, have a nice day Traveler! See ya!')
+    print('Okay then, have a nice day Traveler! See ya!')
     exit()
 
 print('For the first, let me know how much coins you have.')
 input_answer = input('Can you please tell me how much coins you have? (Yes/No): ').strip().lower()
 if input_answer in ('yes', 'y'):
-    print('Oh thanks, Let me check...')
+    print('Oh thanks, let me check...')
     print('You have', coins, 'coins.')
 else:
     print('Alright, maybe next time. See ya!')
@@ -24,7 +24,6 @@ while True:
         print('Sure! Here the availabe Equipment Items that we have:')
         for key, item in items_equipment.items():
             print(f'{key}: {item.name} - {item.price}')
-        # prompt once after listing all items
         selection = input('Select the number of the item you want to buy, Traveler:').strip()
         if selection in items_equipment:
             selected_item = items_equipment[selection]
@@ -42,7 +41,6 @@ while True:
         print('Sure! Here the availabe Food Items that we have:')
         for key, item in items_food.items():
             print(f'{key}: {item.name} - {item.price}')
-        # prompt once after listing all items
         selection = input('Select the number of the item you want to buy, Traveler:').strip()
         if selection in items_food:
             selected_item = items_food[selection]
