abda97d4a1a9d1fdb2b7cb9d0aa4f77b5fe1add6
melodimistis <azaralfy01@gmail.com>
2025-12-30 07:47:16 +0700

    make a python merchant rpg

---
+++ DataItems.py
@@ -0,0 +1,44 @@
+import random
+
+coins = random.randint(1, 10000)
+
+class Player:
+    def __init__ (self, name, coins):
+        self.name = name
+        self.coins = coins
+
+usr = (Player('Traveler', coins))
+
+class Equipment:
+    def __init__ (self, name, price):
+        self.name = name
+        self.price = price
+
+items_equipment = {
+    '1': Equipment('Iron Sword', 399),
+    '2': Equipment('Shield', 500),
+    '3': Equipment('Iron Full Armor', 1500),
+    '4': Equipment('Iron Axe', 450),
+    '5': Equipment('Iron Pickaxe', 400),
+    '6': Equipment('Bow', 220),
+    '7': Equipment('Arrow (x32)', 190),
+    '8': Equipment('Torch (x16)', 50),
+    '9': Equipment('Shovel', 180),
+    '10': Equipment('Hoe', 150),
+    '11': Equipment('Fishing Rod', 240),
+}
+
+class Food:
+    def __init__ (self, name, price):
+        self.name = name
+        self.price = price
+
+items_food = {
+    '1': Food('Bread', 100),
+    '2': Food('Milk', 130),
+    '3': Food('Apple', 50),
+    '4': Food('Cooked Chicken', 180),
+    '5': Food('Chicken Soup', 200),
+    '6': Food('Chicken Grilled', 240),
+    '7': Food('Mushroom Soup', 120),
+}
---
+++ VendingMachine.py
@@ -0,0 +1,63 @@
+from DataItems import coins, usr, items_equipment, items_food
+
+print('Hello Traveler! Welcome to the Medieval Vending Machine.')
+input_ask = input('Can i help you with something today? (I want to buy something/No thanks, maybe next time):').strip().lower()
+if input_ask in ("I want", 'i want', 'i', 'I', 'yes', 'Yes', 'I want to buy something'):
+    print('Great! Let me assist you with that.')
+else:
+    print('Okay the, have a nice day Traveler! See ya!')
+    exit()
+
+print('For the first, let me know how much coins you have.')
+input_answer = input('Can you please tell me how much coins you have? (Yes/No): ').strip().lower()
+if input_answer in ('yes', 'y'):
+    print('Oh thanks, Let me check...')
+    print('You have', coins, 'coins.')
+else:
+    print('Alright, maybe next time. See ya!')
+    exit()
+
+while True:
+    print('Then, what would you like to buy today? In here, we have Equipment and Food items.')
+    choice_category_item = input('What category would you like to choose? (Equipment/Food/Exit):')
+    if choice_category_item == 'Equipment':
+        print('Sure! Here the availabe Equipment Items that we have:')
+        for key, item in items_equipment.items():
+            print(f'{key}: {item.name} - {item.price}')
+        # prompt once after listing all items
+        selection = input('Select the number of the item you want to buy, Traveler:').strip()
+        if selection in items_equipment:
+            selected_item = items_equipment[selection]
+            print('You have selected:', selected_item.name, ', it will cost you', selected_item.price)
+            if usr.coins >= selected_item.price:
+                usr.coins -= selected_item.price
+                print('Thank you for your purchase! Now, you have', usr.coins, 'coins left.')
+            else:
+                print('Sorry, your coins are not enough for this item')
+        else:
+            print('Invalid selection. Please choose a valid item number.')
+            continue
+
+    elif choice_category_item == 'Food':
+        print('Sure! Here the availabe Food Items that we have:')
+        for key, item in items_food.items():
+            print(f'{key}: {item.name} - {item.price}')
+        # prompt once after listing all items
+        selection = input('Select the number of the item you want to buy, Traveler:').strip()
+        if selection in items_food:
+            selected_item = items_food[selection]
+            print('You have selected', selected_item.name, ', it will cost you', selected_item.price)
+            if usr.coins >= selected_item.price:
+                usr.coins -= selected_item.price
+                print('Thank you for your purchase! Now, you have', usr.coins, 'coins left')
+            else:
+                print('Sorry, your coins are not enough for this item')
+        else:
+            print('Invalid selection. Please choose a valid item number.')
+            continue
+    elif choice_category_item == 'Exit':
+        print('Thank you for visiting the Medieval Vending Machine. See you next time, Traveler!')
+        break
+    else:
+        print('Please choose a valid category: Equipment, Food, or Exit.')
+        continue
